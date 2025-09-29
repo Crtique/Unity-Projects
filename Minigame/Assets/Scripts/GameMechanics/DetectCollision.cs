6850e285e4415110845c29f605a3210388ed3d18
Crtique <robinsoncar345@gmail.com>
2025-09-29 11:02:30 -0400

    Updated Code

--- Minigame/Assets/Scripts/GameMechanics/DetectCollision.cs
+++ Minigame/Assets/Scripts/GameMechanics/DetectCollision.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    public int playerHealth = 100;
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if the enemy has been hit by the player
@@ -18,8 +20,18 @@ public class DetectCollision : MonoBehaviour
         {
             Debug.Log("Player Hit!");
 
-            //Destroy(gameObject);
             Destroy(other.gameObject); // Destroy the bullet
+
+            // Decrease health
+            playerHealth--;
+
+            // Check when the players health drops to zero or less when it does destroy/kill player
+            if (playerHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
+        
+        
     }
 }
